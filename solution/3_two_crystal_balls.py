b42e28428bf3064acec5a9c0efb7bed97c206d22
Qianxy <xyqian67@gmail.com>
2022-10-18 14:56:26 +0800

    Add solutions

--- solution/3_two_crystal_balls.py
+++ solution/3_two_crystal_balls.py
@@ -1,4 +1,20 @@
 class Search:
 
     def two_crystal_balls(self, breaks):
-        pass
+        import math
+        step = int(math.sqrt(len(breaks)))
+
+        i = step
+        while i < len(breaks):
+            if breaks[i]:
+                break
+            i += step
+
+        i -= step
+        j = 0
+        while j < step and i < len(breaks):
+            if breaks[i]:
+                return i
+            i += 1
+            j += 1
+        return None
--- solution/4_bubble_sort.py
+++ solution/4_bubble_sort.py
@@ -1,4 +1,8 @@
 class Sort:
 
     def bubble_sort(self, nums):
-        pass
+        for i in range(len(nums)):
+            for j in range(len(nums) - 1 - i):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        return nums
