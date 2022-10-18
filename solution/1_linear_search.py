1ecf88c20d5dd3714d15d6ed49b04108460e1d6e
Qianxy <xyqian67@gmail.com>
2022-10-18 17:50:59 +0800

    Remove redundant problems for test

--- solution/1_linear_search.py
+++
@@ -1,7 +0,0 @@
-class Search:
-
-    def linear_search(self, nums, target):
-        for i, n in enumerate(nums):
-            if n == target:
-                return i
-        return None
--- solution/4_bubble_sort.py
+++
@@ -1,8 +0,0 @@
-class Sort:
-
-    def bubble_sort(self, nums):
-        for i in range(len(nums)):
-            for j in range(len(nums) - 1 - i):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-        return nums
