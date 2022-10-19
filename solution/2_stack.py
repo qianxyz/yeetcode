a6eeab8869aaaa4ebda13f054832d01837e5bdf3
Qianxy <xyqian67@gmail.com>
2022-10-19 15:38:13 +0800

    Add stack problem

---
+++ solution/2_stack.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class Stack:
+
+    def __init__(self) -> None:
+        self.stk = []
+
+    def is_empty(self) -> bool:
+        return not self.stk
+
+    def push(self, item: int) -> None:
+        self.stk.append(item)
+
+    def pop(self) -> Optional[int]:
+        try:
+            return self.stk.pop()
+        except IndexError:
+            return None
+
+    def peek(self) -> Optional[int]:
+        try:
+            return self.stk[-1]
+        except IndexError:
+            return None
