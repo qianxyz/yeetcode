5d0760a9fef3014d0885a8bcf37aa16c4a5754ec
Qianxy <xyqian67@gmail.com>
2022-10-19 15:37:54 +0800

    Refactor to two classes

--- yeetcode.py
+++ yeetcode.py
@@ -4,12 +4,6 @@ import sys
 import yaml
 
 
-_PROBLEM_LIST = "problem.yaml"
-_CONFIG_PATH = "problem/{}.yaml"
-_SOLUTION_MODULE = "solution.{}"
-_SOLUTION_FILE_PATH = "solution/{}.py"
-
-
 def eprint(*args, **kwargs):
     """Print to stderr."""
     print(*args, file=sys.stderr, **kwargs)
@@ -17,39 +11,99 @@ def eprint(*args, **kwargs):
 
 class Problem:
 
-    def __init__(self) -> None:
-        raise NotImplementedError
+    _SOLUTION_MODULE = "solution.{}_{}"
+    _SOLUTION_FILE_PATH = "solution/{}_{}.py"
+
+    def __init__(self, pid: int, name: str,
+                 clazz: str, method: dict, test: list) -> None:
+        self.pid = pid
+        self.name = name
+        self.sol_mod = self._SOLUTION_MODULE.format(pid, name)
+        self.sol_pth = self._SOLUTION_FILE_PATH.format(pid, name)
+        self.clazz = clazz
+        self.method = method
+        self.test = test
 
     def generate_py(self):
-        raise NotImplementedError
+        """Generate a template solution file."""
+        import_optional = False
+
+        src = f"""\
+class {self.clazz}:
+"""
+        for name, args in self.method.items():
+
+            # check if we need to from typing import Optional
+            types = args.values()
+            if any('Optional' in t for t in types if t is not None):
+                import_optional = True
+
+            ret_type = args.pop('__return')
+            arg_str = ", ".join(
+                ["self"] + [f"{k}: {v}" for k, v in args.items()]
+            )
+            src += f"""\
+
+    def {name}({arg_str}) -> {ret_type}:
+        pass
+"""
+
+        if import_optional:
+            src = "from typing import Optional\n\n\n" + src
+
+        if os.path.exists(self.sol_pth):
+            choice = input(f"{self.sol_pth} already exists. Overwrite? [y/n] ")
+            if choice.lower() not in {'y', 'ye', 'yes'}:
+                print("Abort.")
+                exit(0)
+
+        with open(self.sol_pth, 'w') as f:
+            f.write(src)
+        print(f"template generated at {self.sol_pth}")
 
     def run(self):
         raise NotImplementedError
 
 
-class ProblemFactory:
+class ProblemList:
+
+    _PROBLEM_LIST = "problem.yaml"
+    _CONFIG_TEMPLATE = """"""  # TODO
+    _CONFIG_PATH = "problem/{}_{}.yaml"
 
     def __init__(self) -> None:
-        with open(_PROBLEM_LIST) as f:
-            self.plist = yaml.safe_load(f)
+        with open(self._PROBLEM_LIST) as f:
+            self.plist: dict = yaml.safe_load(f)
 
     def get_problem(self, pid: int) -> Problem:
+        """Factory method to get a problem instance."""
         name = self.plist[pid]
+        cfg_path = self._CONFIG_PATH.format(pid, name)
+        with open(cfg_path) as f:
+            cfg: dict = yaml.safe_load(f)
+
+        test = cfg.pop('__test')
+        assert len(cfg) == 1, "too many classes"
+        [(clazz, method)] = list(cfg.items())
+        if len(method) == 1:
+            return SingleMethodProblem(pid, name, clazz, method, test)
+        else:
+            return MultiMethodProblem(pid, name, clazz, method, test)
 
     def add_problem(self, name):
-        max_id = max(self.plist)
-        self.plist[max_id + 1] = name
-        with open(_PROBLEM_LIST, 'w') as f:
+        new_id = max(self.plist) + 1
+        self.plist[new_id] = name
+        with open(self._PROBLEM_LIST, 'w') as f:
             yaml.safe_dump(self.plist, f)
 
-        cfg_path = _CONFIG_PATH.format(self.get_title(max_id + 1))
+        cfg_path = self._CONFIG_PATH.format(new_id, name)
         with open(cfg_path, 'w') as f:
             f.write(self._CONFIG_TEMPLATE)
         print(f"config template generated at {cfg_path}")
 
     def run_all(self):
         for pid in self.plist:
-            problem = Problem(pid, self)
+            problem = self.get_problem(pid)
             problem.run()
 
     def __str__(self) -> str:
@@ -58,72 +112,37 @@ class ProblemFactory:
 
 class SingleMethodProblem(Problem):
 
-    def __init__(self, pid: int, plist: ProblemList) -> None:
-        self.title = plist.get_title(pid)
-        with open(_CONFIG_PATH.format(self.title)) as f:
-            self.cfg: dict = yaml.safe_load(f)
-
-        self.test = self.cfg.pop('__test')
-        # assure that self.cfg only has one key left
-        [(self.clazz, self.method)] = list(self.cfg.items())
-
-        self.has_multiple_methods = (len(self.method) > 1)
-
-    def generate(self):
-        """Generate a template solution file."""
-        src = [f"class {self.clazz}:", ""]
-        for name, args in self.method.items():
-            ret_type = args.pop('__return')
-            args = ", ".join(
-                ["self"] + [f"{k}: {v}" for k, v in args.items()]
-            )
-            src += [
-                f"    def {name}({args}) -> {ret_type}:",
-                "        pass", ""
-            ]
-        src = '\n'.join(src)
-
-        py_path = _SOLUTION_FILE_PATH.format(self.title)
-        if os.path.exists(py_path):
-            choice = input(f"{py_path} already exists. Overwrite? [y/n] ")
-            if choice.lower() not in {'y', 'ye', 'yes'}:
-                print("Abort.")
-                exit(0)
-
-        with open(py_path, 'w') as f:
-            f.write(src)
-        print(f"template generated at {py_path}")
-
     def run(self):
-        sol_class = self.cfg['solution']['class']
-        sol_method = self.cfg['solution']['method']['name']
-
         # import the test function
-        py_mod = _SOLUTION_MODULE.format(self.title)
-        py_mod = importlib.import_module(py_mod)
-        solver = getattr(py_mod, sol_class)()
-        test_func = getattr(solver, sol_method)
+        py_mod = importlib.import_module(self.sol_mod)
+        solver = getattr(py_mod, self.clazz)()
+        [test_func] = list(self.method.keys())
+        test_func = getattr(solver, test_func)
 
         # run test
-        test_cases = self.cfg['test cases']
-        for case in test_cases:
-            input = case['input']
-            expect = case['expect']
-            assert test_func(*input) == expect, "Test failed"
+        for kwargs in self.test:
+            expect = kwargs.pop('__return')
+            assert test_func(**kwargs) == expect, "Test failed"
         print("Test passed")
+
+
+class MultiMethodProblem(Problem):
+
+    def run(self):
+        pass  # TODO
 
 
 if __name__ == '__main__':
     subcommand = sys.argv[1]
     if subcommand == 'generate':
         pid = int(sys.argv[2])
-        Problem(pid, ProblemList()).generate()
+        ProblemList().get_problem(pid).generate_py()
     elif subcommand == 'run':
         if len(sys.argv) <= 2:
             ProblemList().run_all()
         else:
             pid = int(sys.argv[2])
-            Problem(pid, ProblemList()).run()
+            ProblemList().get_problem(pid).run()
     elif subcommand == 'list':
         print(ProblemList())
     elif subcommand == 'add':
