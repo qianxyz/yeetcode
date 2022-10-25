70f79dd781d52959af36fae7d839ad0d20f78203
Qianxy <xyqian67@gmail.com>
2022-10-26 01:33:07 +0800

    Rewrite loader_test.py

--- tests/example.py
+++
@@ -1,3 +0,0 @@
-class Greeting:
-    def hello(self, name: str) -> str:
-        return f"Hello, {name}!"
--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,11 +1,33 @@
-import os.path as osp
+import os
 from yeetcode.loader import load_problem, load_module
+
+# NOTE: These tests create temporary files and remove them later.
+# For `load_problem` a workaround is to pass in raw yaml directly;
+# However, for `load_module`, there seems no way to load python source
+# without calling `exec` (a lot more dangerous than creating tmp files).
 
 
 def test_load_problem():
-    dir = osp.dirname(osp.abspath(__file__))
-    example_yaml = osp.join(dir, "example.yaml")
-    problem = load_problem(example_yaml)
+    yaml_str = """
+Greeting:
+  hello:
+    name: str
+    return: str
+
+tests:
+- name: "Jack"
+  return: "Hello, Jack!"
+- name: "World"
+  return: "Hello, World!"
+"""
+
+    dir = os.path.dirname(os.path.abspath(__file__))
+    tmp_yaml = os.path.join(dir, "tmp.yaml")
+    with open(tmp_yaml, "w") as f:
+        f.write(yaml_str)
+    problem = load_problem(tmp_yaml)
+    os.remove(tmp_yaml)
+
     assert problem.class_name == "Greeting"
     assert problem.methods == {"hello": {"name": "str", "return": "str"}}
     assert problem.test_cases == [
@@ -15,8 +37,17 @@ def test_load_problem():
 
 
 def test_load_module():
-    dir = osp.dirname(osp.abspath(__file__))
-    example_py = osp.join(dir, "example.py")
-    module = load_module(example_py)
+    py_str = """
+class Greeting:
+    def hello(self, name: str) -> str:
+        return f"Hello, {name}!"
+"""
+
+    dir = os.path.dirname(os.path.abspath(__file__))
+    tmp_py = os.path.join(dir, "tmp.py")
+    with open(tmp_py, "w") as f:
+        f.write(py_str)
+    module = load_module(tmp_py)
     greeting = module.Greeting()
     assert greeting.hello("World") == "Hello, World!"
+    os.remove(tmp_py)
