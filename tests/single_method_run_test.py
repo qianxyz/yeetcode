36a65f6103e8511beb7d0ea3305700e0be1dd7fc
Qianxy <xyqian67@gmail.com>
2022-10-25 17:56:11 +0800

    Add single_method_run_test.py

---
+++ tests/single_method_run_test.py
@@ -0,0 +1,34 @@
+import sys
+import pytest
+from yeetcode.problem import SingleMethodProblem
+
+
+class Addition:
+    def sum_of_two(self, a: int, b: int) -> int:
+        return a + b
+
+
+class WrongAddition:
+    def sum_of_two(self, a: int, b: int) -> int:
+        return a * b
+
+
+def single_method_problem(name: str) -> SingleMethodProblem:
+    class_name = name
+    methods = {"sum_of_two": {"a": "int", "b": "int", "__return": "int"}}
+    test_cases = [
+        {"a": 0, "b": 1, "__return": 1},
+        {"a": 2, "b": 2, "__return": 4},
+    ]
+    return SingleMethodProblem(class_name, methods, test_cases)
+
+
+def test_good_run():
+    problem = single_method_problem("Addition")
+    problem.run_tests(sys.modules[__name__])
+
+
+def test_bad_run():
+    problem = single_method_problem("WrongAddition")
+    with pytest.raises(AssertionError):
+        problem.run_tests(sys.modules[__name__])
