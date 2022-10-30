2a391a64b2000668ff37c4cf6a0908817b59421a
Qianxy <xyqian67@gmail.com>
2022-10-30 20:49:16 +0800

    Change linked list example problem

--- example/solution/3_merge_two_sorted_lists.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from yeetcode import ListNode
-
-
-class Solution:
-    def merge_two_sorted_lists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        pass
---
+++ example/solution/3_reverse_linked_list.py
@@ -0,0 +1,7 @@
+from typing import List, Optional
+from yeetcode import ListNode
+
+
+class Solution:
+    def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
