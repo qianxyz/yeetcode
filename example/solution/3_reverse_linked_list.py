27c32c9eff5ed1a5bff8537173ba6f25ad31f88f
Qianxy <xyqian67@gmail.com>
2022-10-30 20:53:39 +0800

    Finish linked list example problem

--- example/solution/3_reverse_linked_list.py
+++ example/solution/3_reverse_linked_list.py
@@ -4,4 +4,9 @@ from yeetcode import ListNode
 
 class Solution:
     def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if head is None or head.next is None:
+            return head
+        h = self.reverse_list(head.next)
+        head.next.next = head
+        head.next = None
+        return h
